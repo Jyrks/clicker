23f2434e27d6a854d37c3df93202614b212ee412
Jyrks <hampsu17@gmail.com>
2020-02-10 19:12:07 +0200

    Added delay and enable

--- src/main/java/nisu/JavaSoundRecorder.java
+++ src/main/java/nisu/JavaSoundRecorder.java
@@ -8,8 +8,8 @@ import javax.sound.sampled.TargetDataLine;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.InputEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+
+import static org.apache.commons.lang3.math.NumberUtils.isCreatable;
 
 public class JavaSoundRecorder {
 
@@ -23,29 +23,40 @@ public class JavaSoundRecorder {
         return format;
     }
 
-    void start() throws LineUnavailableException, AWTException {
+    void start() throws LineUnavailableException, AWTException, InterruptedException {
         AudioFormat audioFormat = getAudioFormat();
         TargetDataLine targetDataLine = AudioSystem.getTargetDataLine(audioFormat);
         targetDataLine.open();
         targetDataLine.start();
 
         JFrame jf = new JFrame();
-        jf.add(new JLabel("Label"));
+
+        JLabel label  = new JLabel("Delay");
+        label.setBounds(160, 0, 50, 30);
+        jf.add(label);
+
+        JTextField textField = new JTextField(30);
+        textField.setBounds(120, 0, 35, 30);
+        jf.add(textField);
+
+        JToggleButton enableButton = new JToggleButton("Enable");
+        enableButton.setBounds(120,50,80,30);
+        jf.add(enableButton);
+
         JPanel jp = new MotionPanel(jf);
         jf.setUndecorated(true);
-        jp.setPreferredSize(new Dimension(110, 110));
+        jp.setPreferredSize(new Dimension(200, 102));
         jp.setOpaque(false);
         jf.getContentPane().add(jp);
         jf.pack();
         jf.setVisible(true);
         jf.setBackground(new Color(0, 0, 0, 1));
+        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         byte[] buffer = new byte[2000];
         while (true) {
             int bytesRead = targetDataLine.read(buffer, 0, buffer.length);
-
             short max;
-
 
             if (bytesRead >= 0) {
                 max = (short) (buffer[0] + (buffer[1] << 8));
@@ -53,20 +64,26 @@ public class JavaSoundRecorder {
                     short thisValue = (short) (buffer[p] + (buffer[p + 1] << 8));
                     if (thisValue > max) max = thisValue;
                 }
-                System.out.println(max);
-                System.out.println(jf.getLocationOnScreen());
-                if (max > 2000) {
+                if (max > 2000 && enableButton.isSelected()) {
+                    Thread.sleep(getDelay(textField));
                     Robot bot = new Robot();
                     moveMouse(jf.getLocationOnScreen().x + 50, jf.getLocationOnScreen().y + 50, 4, bot);
-                    jf.hide();
+                    jf.setVisible(false);
                     bot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
                     bot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-                    jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-                    jf.dispatchEvent(new WindowEvent(jf, WindowEvent.WINDOW_CLOSING));
-                    return;
+                    jf.setVisible(true);
+                    enableButton.setSelected(false);
                 }
             }
         }
+    }
+
+    public static Long getDelay(JTextField textField) {
+        String text = textField.getText().replace(",", ".");
+        if (!isCreatable(text)) {
+            return 0L;
+        }
+        return new Double(Double.parseDouble(text) * 1000).longValue();
     }
 
     public static void moveMouse(int x, int y, int maxTimes, Robot screenWin) {
